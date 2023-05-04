f52cbb06cfc9c31896a2dbc54a653054bfbe5168
Mochi Xu <mochix@moqi.ai>
2023-05-04 10:02:26 +0800

    add exp for weaviate

--- engine/clients/weaviate/search.py
+++ engine/clients/weaviate/search.py
@@ -26,39 +26,43 @@ class WeaviateSearcher(BaseSearcher):
 
     @classmethod
     def search_one(cls, vector, meta_conditions, top, schema) -> List[Tuple[int, float]]:
-        where_conditions = cls.parser.parse(meta_conditions)
-        query = cls.client.query.get(
-            WEAVIATE_CLASS_NAME, ["_additional {id distance}"]
-            # WEAVIATE_CLASS_NAME, ["_additional {id vector distance}", "probability"]  # for debug, payload vector
-        ).with_near_vector({"vector": vector})
+        while True:
+            try:
+                where_conditions = cls.parser.parse(meta_conditions)
+                query = cls.client.query.get(
+                    WEAVIATE_CLASS_NAME, ["_additional {id distance}"]
+                    # WEAVIATE_CLASS_NAME, ["_additional {id vector distance}", "probability"]  # for debug, payload vector
+                ).with_near_vector({"vector": vector})
 
-        is_geo_query = False  # geo 数据集目前未测试, 未支持
-        if where_conditions is not None:
-            operands = where_conditions["operands"]
-            is_geo_query = any(
-                operand["operator"] == "WithinGeoRange" for operand in operands
-            )
-            query = query.with_where(where_conditions)
-            # print(f"weaviate search with filter: {where_conditions}")
+                is_geo_query = False  # geo 数据集目前未测试, 未支持
+                if where_conditions is not None:
+                    operands = where_conditions["operands"]
+                    is_geo_query = any(
+                        operand["operator"] == "WithinGeoRange" for operand in operands
+                    )
+                    query = query.with_where(where_conditions)
+                    # print(f"weaviate search with filter: {where_conditions}")
 
-        query_obj = query.with_limit(top)
-        if is_geo_query:
-            # weaviate can't handle geo queries in python due to excess quotes in generated queries
-            gql_query = query_obj.build()
-            for field in ("geoCoordinates", "latitude", "longitude", "distance", "max"):
-                gql_query = gql_query.replace(f'"{field}"', field)  # get rid of quotes
-            response = cls.client.query.raw(gql_query)
-        else:
-            response = query_obj.do()
-        res = response["data"]["Get"][WEAVIATE_CLASS_NAME]
+                query_obj = query.with_limit(top)
+                if is_geo_query:
+                    # weaviate can't handle geo queries in python due to excess quotes in generated queries
+                    gql_query = query_obj.build()
+                    for field in ("geoCoordinates", "latitude", "longitude", "distance", "max"):
+                        gql_query = gql_query.replace(f'"{field}"', field)  # get rid of quotes
+                    response = cls.client.query.raw(gql_query)
+                else:
+                    response = query_obj.do()
+                res = response["data"]["Get"][WEAVIATE_CLASS_NAME]
 
-        id_score_pairs: List[Tuple[int, float]] = []
-        for obj in res:
-            description = obj["_additional"]
-            score = description["distance"]
-            id_ = uuid.UUID(hex=description["id"]).int
-            id_score_pairs.append((id_, score))
-        return id_score_pairs
+                id_score_pairs: List[Tuple[int, float]] = []
+                for obj in res:
+                    description = obj["_additional"]
+                    score = description["distance"]
+                    id_ = uuid.UUID(hex=description["id"]).int
+                    id_score_pairs.append((id_, score))
+                return id_score_pairs
+            except Exception as e:
+                print(f"weaviate search exception is {e}")
 
     def setup_search(self, host, distance, connection_params: dict, search_params: dict):
         search_params = {
