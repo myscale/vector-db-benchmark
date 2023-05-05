e3058a4042e505eedaf138070c63d9c9d077cecf
Mochi Xu <mochix@moqi.ai>
2023-05-05 17:38:50 +0800

    change weaviate upload

--- engine/clients/weaviate/upload.py
+++ engine/clients/weaviate/upload.py
@@ -1,3 +1,4 @@
+import time
 import uuid
 from typing import List, Optional
 
@@ -35,20 +36,22 @@ class WeaviateUploader(BaseUploader):
     def upload_batch(
             cls, ids: List[int], vectors: List[list], metadata: List[Optional[dict]]
     ):
-        cls.client.batch.configure(batch_size=cls.upload_params.get("batch_size", 100), timeout_retries=3)
-        while True:
-            try:
-                with cls.client.batch as batch:
-                    for id_, vector, data_object in zip(ids, vectors, metadata):
-                        # Handling latitude and longitude data
-                        data_object = cls._update_geo_data(data_object)
+        cls.client.batch.configure(batch_size=cls.upload_params.get("batch_size", 100), timeout_retries=3, dynamic=True)
+
+        with cls.client.batch as batch:
+            for id_, vector, data_object in zip(ids, vectors, metadata):
+                # Handling latitude and longitude data
+                data_object = cls._update_geo_data(data_object)
+                while True:
+                    try:
                         batch.add_data_object(
                             data_object=data_object or {},
                             class_name=WEAVIATE_CLASS_NAME,
                             uuid=uuid.UUID(int=id_).hex,
                             vector=vector,
                         )
-                    batch.create_objects()
-                break
-            except Exception as e:
-                print(e)
+                        break
+                    except Exception as e:
+                        print(f"weaviate upload exp: {e}")
+                        time.sleep(3)
+            # batch.create_objects()
