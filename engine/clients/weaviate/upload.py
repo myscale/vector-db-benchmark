1df6476ee8b9c9d94bc951910d77c67bf4cf45c7
Mochi Xu <mochix@moqi.ai>
2023-05-08 11:29:17 +0800

    fix weaviate batch client OOM

--- engine/clients/weaviate/upload.py
+++ engine/clients/weaviate/upload.py
@@ -11,13 +11,26 @@ from engine.clients.weaviate.config import WEAVIATE_CLASS_NAME, WEAVIATE_DEFAULT
 class WeaviateUploader(BaseUploader):
     client = None
     upload_params = {}
+    connection_params = {}
+    host = "127.0.0.1"
 
     @classmethod
     def init_client(cls, host, distance, connection_params, upload_params,
                     extra_columns_name: list, extra_columns_type: list):
         cls.client = generateWeaviateClient(connection_params={**connection_params}, host=host)
         cls.upload_params = upload_params
+        cls.connection_params = connection_params
+        cls.host = host
 
+    @classmethod
+    def re_connect(cls):
+        while True:
+            try:
+                cls.client = generateWeaviateClient(connection_params={**cls.connection_params}, host=cls.host)
+                break
+            except Exception as e:
+                print(f"re connect exp {e}")
+                time.sleep(3)
     @staticmethod
     def _update_geo_data(data_object):
         if data_object is None:
@@ -36,22 +49,26 @@ class WeaviateUploader(BaseUploader):
     def upload_batch(
             cls, ids: List[int], vectors: List[list], metadata: List[Optional[dict]]
     ):
-        cls.client.batch.configure(batch_size=cls.upload_params.get("batch_size", 100), timeout_retries=3, dynamic=True)
-
-        with cls.client.batch as batch:
-            for id_, vector, data_object in zip(ids, vectors, metadata):
-                # Handling latitude and longitude data
-                data_object = cls._update_geo_data(data_object)
-                while True:
-                    try:
+        # cls.client.batch.configure(batch_size=cls.upload_params.get("batch_size", 100), timeout_retries=3, dynamic=True)
+        cls.client.batch.configure(batch_size=None, timeout_retries=3, dynamic=False)
+        while True:
+            try:
+                with cls.client.batch as batch:
+                    for id_, vector, data_object in zip(ids, vectors, metadata):
+                        # Handling latitude and longitude data
+                        data_object = cls._update_geo_data(data_object)
                         batch.add_data_object(
                             data_object=data_object or {},
                             class_name=WEAVIATE_CLASS_NAME,
                             uuid=uuid.UUID(int=id_).hex,
                             vector=vector,
                         )
-                        break
-                    except Exception as e:
-                        print(f"weaviate upload exp: {e}")
-                        time.sleep(3)
-            # batch.create_objects()
+                    batch.create_objects()
+                    break
+            except Exception as e:
+                print(f"weaviate batch exp: {e}")
+                time.sleep(3)
+                cls.re_connect()
+
+
+
