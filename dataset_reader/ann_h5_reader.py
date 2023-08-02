af6db47a2a01ac66f332390596da9c2fd7c5a184
Mochi Xu <mochix@moqi.ai>
2023-08-02 15:38:20 +0800

    refined ann h5 reader

--- dataset_reader/ann_h5_reader.py
+++ dataset_reader/ann_h5_reader.py
@@ -1,4 +1,5 @@
 import ast
+import math
 from typing import Iterator, Optional, Tuple
 
 import h5py
@@ -8,7 +9,7 @@ from benchmark.dataset_config import DatasetConfig
 from dataset_reader.base_reader import BaseReader, Query, Record
 from dataset_reader.utils import convert_H52py
 
-
+HDF5_BATCH_PART_SIZE = 1000000
 class AnnH5Reader(BaseReader):
     def __init__(self, dataset_dir, dataset_config: DatasetConfig, normalize=False):
         self.dataset_dir = dataset_dir
@@ -59,19 +60,41 @@ class AnnH5Reader(BaseReader):
         with h5py.File(self.dataset_dir / self.dataset_config.path, "r") as train_data:
             extra_columns = train_data.attrs.get("extra_columns", [])
             extra_columns_type = train_data.attrs.get("extra_columns_type", [])
-            for idx, vector in enumerate(train_data["train"]):
-                # normalize the vector for some distance
-                if self.normalize:
-                    vector /= np.linalg.norm(vector)
+            # get origin train datasets length
+            data_size = train_data["train"].shape[0]
+            # default use one batch_part
+            batch_parts = 1
 
-                # read payload data
-                extra_columns_data = {}
-                for col_name, col_type in zip(extra_columns, extra_columns_type):
-                    extra_columns_data[col_name] = convert_H52py(col_type)(train_data[col_name][idx])
+            if data_size > HDF5_BATCH_PART_SIZE:
+                batch_parts = math.ceil(data_size / HDF5_BATCH_PART_SIZE)
+            block_size = data_size // batch_parts
 
-                yield Record(id=idx,
-                             vector = vector.tolist() if (len(vector)==self.dataset_config.vector_size) else np.random.uniform(0, 1, self.dataset_config.vector_size).tolist(),
-                             metadata=None if len(extra_columns_data.keys()) == 0 else extra_columns_data)
+            for i in range(batch_parts):
+                print(f"\nbatch_part: cur-{i+1}/total-{batch_parts}, data_size:{data_size}, HDF5_BATCH_PART_SIZE: {HDF5_BATCH_PART_SIZE}")
+                start = i * block_size
+                # To handle the case of uneven data sizes, we allow the last block to contain all the remaining data.
+                if i == batch_parts - 1:
+                    end = data_size
+                else:
+                    end = start + block_size
+                # avoid mess memory consume
+                data_block = train_data["train"][start:end]
+
+                for idx, vector in enumerate(data_block):
+                    # normalize the vector for some distance
+                    if self.normalize:
+                        vector /= np.linalg.norm(vector)
+
+                    # read payload data
+                    extra_columns_data = {}
+                    for col_name, col_type in zip(extra_columns, extra_columns_type):
+                        extra_columns_data[col_name] = convert_H52py(col_type)(train_data[col_name][idx])
+
+                    yield Record(id=idx,
+                                 vector=vector.tolist()
+                                 if (len(vector) == self.dataset_config.vector_size)
+                                 else np.random.uniform(0, 1, self.dataset_config.vector_size).tolist(),
+                                 metadata=None if len(extra_columns_data.keys()) == 0 else extra_columns_data)
 
     def read_column_name_type(self) -> Tuple[list, list]:
         """ Get the payloads data name and type """
